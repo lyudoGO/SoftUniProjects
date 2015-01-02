03253b2cce749da03b461c9aea5390738a1a2dea
Lyudmil <llgg2312@gmail.com>
2015-01-02 19:17:10 +0200

    Create app.js

--- SPA-Ads-Project-AngularJS/js/app.js
+++ SPA-Ads-Project-AngularJS/js/app.js
@@ -1,3 +1,17 @@
 'use strict';
 
-var adsApp = angular.module('adsApp', []);
+var adsApp = angular.module('adsApp', ['ngRoute'])
+.config(function($routeProvider) {
+	$routeProvider.when('/ads', {
+		templateUrl: 'templates/home.html'
+	});
+	$routeProvider.when('/login', {
+		templateUrl: 'templates/login.html'
+	});
+	$routeProvider.when('/register', {
+		templateUrl: 'templates/registretion.html'
+	});
+	$routeProvider.otherwise('/register', {
+		redirectTo: '/ads'
+	});
+});
