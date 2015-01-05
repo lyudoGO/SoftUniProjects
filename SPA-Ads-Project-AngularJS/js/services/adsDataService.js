fd059b8756144a47e702a9c845661b8ec1b6c9ef
Lyudmil <llgg2312@gmail.com>
2015-01-05 21:50:47 +0200

    Rename and update services

--- SPA-Ads-Project-AngularJS/js/services/adsDataService.js
+++ SPA-Ads-Project-AngularJS/js/services/adsDataService.js
@@ -1,11 +1,12 @@
 'use strict';
 
-adsApp.factory('adsData', function ($http, $log) {
+adsApp.factory('adsDataService', function ($http, $log) {
 		
-	var getAllAds = function getAllAds(success) {
+	var getAllAds = function getAllAds(categoryId, townId, startPage, pageSize, success) {
 		$http({
 				method: 'GET',
-				url: 'http://softuni-ads.azurewebsites.net/api/ads'
+				url: 'http://softuni-ads.azurewebsites.net/api/Ads?CategoryId=' + categoryId +
+				'&TownId=' + townId + '&StartPage=' + startPage + '&PageSize=' + pageSize
 		})
 		.success(function(data, status, headers, config) {
 			success(data);
@@ -14,6 +15,19 @@ adsApp.factory('adsData', function ($http, $log) {
 			$log.warn(data);
 		})
 	}
+
+/*	var getAdsByCategory = function getAdsByCategory(categoryId, success) {
+		$http({
+				method: 'GET',
+				url: 'http://softuni-ads.azurewebsites.net/api/ads?categoryId=' + categoryId
+		})
+		.success(function(data, status, headers, config) {
+			success(data);
+		})
+		.error(function(data, status, headers, config) {
+			$log.warn(data);
+		})
+	}*/
 
 	var getAllTowns = function getAllTowns(success) {
 		$http({
