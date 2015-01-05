2a83ed00886254fd2b00f3ce9305add0e1c06fe2
Lyudmil <llgg2312@gmail.com>
2015-01-05 21:49:27 +0200

    Update app.js

--- SPA-Ads-Project-AngularJS/js/app.js
+++ SPA-Ads-Project-AngularJS/js/app.js
@@ -3,18 +3,30 @@
 var adsApp = angular.module('adsApp', ['ngRoute'])
 .config(function($routeProvider) {
 	$routeProvider.when('/', {
-		templateUrl: 'templates/home.html'
+		templateUrl: 'templates/home.html',
+		controller: 'AdsController'
 	});
-	$routeProvider.when('/ads', {
-		templateUrl: 'templates/home.html'
+	$routeProvider.when('/category/:categoriesId', {
+		templateUrl: 'templates/home.html',
+		controller: 'AdsController'
+	});
+	$routeProvider.when('/town/:townsId', {
+		templateUrl: 'templates/home.html',
+		controller: 'AdsController'
+	});
+		$routeProvider.when('/ads/page/:page', {
+		templateUrl: 'templates/home.html',
+		controller: 'AdsController'
 	});
 	$routeProvider.when('/login', {
-		templateUrl: 'templates/login.html'
+		templateUrl: 'templates/login.html',
+		controller: 'AdsController'
 	});
 	$routeProvider.when('/register', {
-		templateUrl: 'templates/registration.html'
+		templateUrl: 'templates/registration.html',
+		controller: 'AdsController'
 	});
 	$routeProvider.otherwise({
-		redirectTo: '/ads'
+		redirectTo: '/'
 	});
 });
