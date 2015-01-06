6b2021586ec42d72c1e73d8748ee4af3077f6215
Lyudmil <llgg2312@gmail.com>
2015-01-06 14:57:06 +0200

    Update user service

--- SPA-Ads-Project-AngularJS/js/services/data/userDataService.js
+++ SPA-Ads-Project-AngularJS/js/services/data/userDataService.js
@@ -3,20 +3,31 @@
 adsApp.factory('userDataService', ['$resource', 'baseServiceUrl', 'authenticationService', function ($resource, baseServiceUrl, authenticationService) {
 	
 	function registerUser(user) {
-		return $resource(baseServiceUrl + 'user/Register')
+		return $resource(baseServiceUrl + 'user/register')
 						.save(user)
 						.$promise
 						.then(function (data) {
-							authenticationService.saveUser(data);
+							authenticationService.saveUser(angular.toJson(data));
+							authenticationService.getHeaders();
 						});
 	}
 
 	function loginUser(user) {
-		return resource.query();
+		return $resource(baseServiceUrl + 'user/login')
+						.save(user)
+						.$promise
+						.then(function (data) {
+							authenticationService.saveUser(angular.toJson(data));
+						});
 	}
 
 	function logoutUser() {
-		return resource.query();
+		return $resource(baseServiceUrl + 'user/logout')
+						.save(user)
+						.$promise
+						.then(function (data) {
+							authenticationService.removeUser();
+						});
 	}
 
 	return {
