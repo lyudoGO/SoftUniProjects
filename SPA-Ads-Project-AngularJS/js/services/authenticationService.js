1d46a5cffb82e5bd6bc857a7ff6713d03f87ed10
Lyudmil <llgg2312@gmail.com>
2015-01-06 14:56:35 +0200

    Creating new service

---
+++ SPA-Ads-Project-AngularJS/js/services/authenticationService.js
@@ -0,0 +1,39 @@
+'use strict';
+
+adsApp.factory('authenticationService', function() {
+	var key = 'user';
+	function saveUser(data) {
+		localStorage.setItem(key, angular.toJson(data))
+	}
+
+	function getUser() {
+		return angular.fromJson(localStorage.getItem(key));
+	}
+
+	function getHeaders(argument) {
+		var headers = {};
+		var userData = getUser();
+		if (userData) {
+			headers.Authorization = 'Bearer ' + getUser().access_token;
+		};
+
+		return headers;
+	}
+
+	function removeUser() {
+		localStorage.removeItem(key);
+	}
+
+	function isAdmin() {
+		var isAdmin = getUser().isAdmin;
+		return isAdmin;
+	}
+
+	return {
+		saveUser: saveUser,
+		getUser: getUser,
+		getHeaders: getHeaders,
+		removeUser: removeUser,
+		isAdmin: isAdmin
+	}
+});
