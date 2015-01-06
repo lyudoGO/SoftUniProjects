0612dfcd28e133b1648d9d33ccd3c6eb02a0a3f5
Lyudmil <llgg2312@gmail.com>
2015-01-06 22:34:46 +0200

    Update login and register controllers

--- SPA-Ads-Project-AngularJS/js/controllers/LoginController.js
+++ SPA-Ads-Project-AngularJS/js/controllers/LoginController.js
@@ -1,8 +1,14 @@
 'use strict';
 
-adsApp.controller('LoginController', ['$scope', 'userDataService', function($scope, userDataService) {
-	$('#title').text('Login');
+adsApp.controller('LoginController', ['$scope', '$location', 'userDataService', function($scope, $location, userDataService) {
+	/*$('#title').text('Login');*/
+	$scope.pageTitle = 'Login';
 	$scope.loginUser = function (user) {
-		userDataService.loginUser(user);
+		userDataService.loginUser(user)
+						.$promise
+						.then(function (data) {
+							$location.path('/');
+						});
 	}
+	console.log($scope.pageTitle);
 }]);
--- SPA-Ads-Project-AngularJS/js/controllers/RegisterController.js
+++ SPA-Ads-Project-AngularJS/js/controllers/RegisterController.js
@@ -1,13 +1,17 @@
 'use strict';
 
 adsApp.controller('RegisterController', ['$scope', 'townsDataService', 'userDataService', function($scope, townsDataService, userDataService) {
-	$('#title').text('Register');
-	townsDataService.getAllTowns()
+	/*$('#title').text('Register');*/
+	$scope.pageTitle = 'Register';
+/*	townsDataService.getAllTowns()
 					.$promise
 					.then(function (data) {
 						$scope.towns = data;
 						console.log(data);
-					});
+					});*/
+	townsDataService.getAllTowns(function(resp) {
+		$scope.towns = resp;
+	});
 	$scope.registerUser = function (user) {
 		//console.log(user);
 		userDataService.registerUser(user);
