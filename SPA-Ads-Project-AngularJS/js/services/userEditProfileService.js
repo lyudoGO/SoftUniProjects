1780a73c4e00db872aa34cccf2fb91841cae16a6
Lyudmil <llgg2312@gmail.com>
2015-01-09 08:45:37 +0200

    Implement user edit profile service

---
+++ SPA-Ads-Project-AngularJS/js/services/userEditProfileService.js
@@ -0,0 +1,54 @@
+'use strict';
+
+adsApp.factory('userEditProfileService', ['$http', '$log', 'baseServiceUrl', function ($http, $log, baseServiceUrl) {
+		
+	function getUserProfile(userAccessToken, success) {
+		$http({
+				method: 'GET',
+				headers: { Authorization: 'Bearer ' + userAccessToken },
+				url: baseServiceUrl + 'user/profile/'
+		})
+		.success(function(data, status, headers, config) {
+			success(data);
+		})
+		.error(function(data, status, headers, config) {
+			$log.error(data);
+		})
+	}
+
+	function editUserProfile(userAccessToken, userData, success) {
+		$http({
+				method: 'PUT',
+				headers: { Authorization: 'Bearer ' + userAccessToken },
+				url: baseServiceUrl + 'user/profile/',
+				data: JSON.stringify(userData)
+		})
+		.success(function(data, status, headers, config) {
+			success(data);
+		})
+		.error(function(data, status, headers, config) {
+			$log.error(data);
+		})
+	}
+
+	function changeUserPassword(userAccessToken, userData, success) {
+		$http({
+				method: 'PUT',
+				headers: { Authorization: 'Bearer ' + userAccessToken },
+				url: baseServiceUrl + 'user/changePassword/',
+				data: JSON.stringify(userData)
+		})
+		.success(function(data, status, headers, config) {
+			success(data);
+		})
+		.error(function(data, status, headers, config) {
+			$log.error(data);
+		})
+	}
+
+	return {
+		getUserProfile: getUserProfile,
+		editUserProfile: editUserProfile,
+		changeUserPassword: changeUserPassword
+	}
+}]);
