e3076052a01e639ddb2a5284fe2ad1822a885830
Lyudmil <llgg2312@gmail.com>
2015-01-09 23:31:35 +0200

    Implement new functionality to categories controller

--- SPA-Ads-Project-AngularJS/js/controllers/CategoriesController.js
+++ SPA-Ads-Project-AngularJS/js/controllers/CategoriesController.js
@@ -4,4 +4,16 @@ adsApp.controller('CategoriesController', ['$scope', 'categoriesDataService', fu
 	categoriesDataService.getAllCategories(function(resp) {
 		$scope.categories = resp;
 	});
+
+	$scope.getCategoryId = function getCategoryId(id, name) {
+		$scope.parameters.categoryId = id;
+		$scope.parameters.categoryName = name;
+		/*alert($scope.parameters.categoryId);*/
+	};
+
+	$scope.cancelCategoryId = function cancelCategoryId() {
+		$scope.parameters.categoryId = '';
+		$scope.parameters.categoryName = '';
+		/*alert($scope.parameters.categoryId);*/
+	}
 }]);
