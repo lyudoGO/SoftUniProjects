1b9b67edf45caa7a2460232d4abe7dd63f1ab058
Lyudmil <llgg2312@gmail.com>
2015-01-10 15:54:11 +0200

    Implement alert message to categories controller

--- SPA-Ads-Project-AngularJS/js/controllers/CategoriesController.js
+++ SPA-Ads-Project-AngularJS/js/controllers/CategoriesController.js
@@ -1,21 +1,24 @@
 'use strict';
 
 adsApp.controller('CategoriesController', ['$scope', 'categoriesDataService', function($scope, categoriesDataService) {
-	categoriesDataService.getAllCategories(function(resp) {
-		$scope.categories = resp;
-	});
+	categoriesDataService.getAllCategories(
+		function(data, status, headers, config) {
+			$scope.alertMsg('success', 'Categories was successfully loaded!');
+			$scope.categories = data;
+		},
+		function (data, status, headers, config) {
+        	$scope.alertMsg('danger', 'Failed to load categories. Please try again later.');
+    	});
 
 	$scope.getCategoryId = function getCategoryId(id, name) {
 		$scope.parameters.categoryId = id;
 		$scope.parameters.categoryName = name;
 		$scope.parameters.startPage = 1;
-		/*alert($scope.parameters.categoryId);*/
 	};
 
 	$scope.cancelCategoryId = function cancelCategoryId() {
 		$scope.parameters.categoryId = '';
 		$scope.parameters.categoryName = '';
 		$scope.parameters.startPage = 1;
-		/*alert($scope.parameters.categoryId);*/
 	}
 }]);
