38437be85bbd80575adbaf15e513a0a04718c63b
Lyudmil <llgg2312@gmail.com>
2015-01-10 15:55:36 +0200

    Implement alert message towns controller

--- SPA-Ads-Project-AngularJS/js/controllers/townsController.js
+++ SPA-Ads-Project-AngularJS/js/controllers/townsController.js
@@ -2,21 +2,24 @@
 
 adsApp.controller('TownsController', ['$scope', 'townsDataService', function($scope, townsDataService) {
 
-	townsDataService.getAllTowns(function(resp) {
-		$scope.towns = resp;
-	});
+	townsDataService.getAllTowns(
+		function(data) {
+			$scope.alertMsg('success', 'Towns was successfully loaded!');
+			$scope.towns = data;
+		},
+		function (data, status, headers, config) {
+        	$scope.alertMsg('danger', 'Failed to load towns. Please try again later.');
+    	});
 
 	$scope.getTownId = function getTownId(id, name) {
 		$scope.parameters.townId = id;
 		$scope.parameters.townName = name;
 		$scope.parameters.startPage = 1;
-		/*alert($scope.parameters.townId);*/
 	}
 
 	$scope.cancelTownId = function cancelTownId() {
 		$scope.parameters.townId = '';
 		$scope.parameters.townName = '';
 		$scope.parameters.startPage = 1;
-		/*alert($scope.parameters.townId);*/
 	}
 }]);
