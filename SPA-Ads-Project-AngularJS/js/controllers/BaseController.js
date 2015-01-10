369668a5361480de18054f59ef5d2407fdd076c8
Lyudmil <llgg2312@gmail.com>
2015-01-10 10:35:18 +0200

    Big fix to base controller

--- SPA-Ads-Project-AngularJS/js/controllers/BaseController.js
+++ SPA-Ads-Project-AngularJS/js/controllers/BaseController.js
@@ -1,5 +1,26 @@
 'use strict';
 
-adsApp.controller('BaseController', ['$scope', function($scope) {
-	$scope.$parent.pageTitle = 'Home';
+adsApp.controller('BaseController', ['$scope', 'authenticationService', function($scope, authenticationService) {
+	$scope.alertMsg = false;
+	$('#title').text('Home');
+	$scope.parameters = {
+		startPage: 1,
+		pageSize: 4,
+		categoryId: '',
+		townId: '',
+		categoryName: '',
+		townName: '',
+		pageTitle: 'Home'
+	};
+
+	if (authenticationService.getUser()) {
+		var userData = angular.fromJson(authenticationService.getUser());
+		$scope.username = userData['username'];
+		$scope.isLogged = authenticationService.isLogged();
+	};
+	
+	$scope.logout = function () {
+		authenticationService.removeUser();
+		$scope.isLogged = false;
+	}
 }]);
