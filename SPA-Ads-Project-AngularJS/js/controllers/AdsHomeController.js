6245b82094c0d503b2317da40d26f411231774ae
Lyudmil <llgg2312@gmail.com>
2015-01-11 09:41:18 +0200

    Big fix to home controller to work with pagination

--- SPA-Ads-Project-AngularJS/js/controllers/AdsHomeController.js
+++ SPA-Ads-Project-AngularJS/js/controllers/AdsHomeController.js
@@ -1,20 +1,53 @@
 'use strict';
 
 adsApp.controller('AdsHomeController', ['$scope', '$routeParams', 'adsDataService', 'authenticationService', function($scope, $routeParams, adsDataService, authenticationService) {
-	/*$('#title').text('Home');*/
-	/*$scope.base.pageTitle = 'Home';*/
-	if (authenticationService.getUser()) {
-		var userData = angular.fromJson(authenticationService.getUser());
-		$scope.username = userData['username'];
-		$scope.isLogged = authenticationService.isLogged();
-	};
-	
-	$scope.logout = function() {
-		authenticationService.removeUser();
-		$scope.isLogged = false;
-	}
+	$('#title').text('Home');
+
+    $scope.totalAds = 0;
+    $scope.pagination = {
+        current: 1
+    };
+
+    $scope.pageChanged = function(newPage) {
+        getAllAds(newPage);
+    };
 
 	$scope.ready = false;
+	$scope.isUserHome = false;
+
+	getAllAds(1);
+
+	$scope.reload = function reload() {
+		$scope.parameters.categoryId = '';
+		$scope.parameters.townId = '';
+		$('#title').text('Home');
+		getAllAds(1);
+	};
+
+	function getAllAds(newPage) {
+		adsDataService.getAllAds($scope.parameters.categoryId, $scope.parameters.townId, newPage, $scope.parameters.pageSize, 
+			function(data, status, headers, config) {
+				$scope.alertMsg('success', 'Ads was successfully loaded!');
+				$scope.data = data;
+				$scope.ready = true;
+				/*$scope.numPages = [];
+				$scope.totalAds = data.numItems;
+				var startWithPage = $scope.parameters.startPage - 1;
+				for (var i = 0; i < 5; i++, startWithPage++) {
+					$scope.numPages[i] = startWithPage + 1;
+				};*/
+			},
+			function (data, status, headers, config) {
+            	$scope.alertMsg('danger', 'Failed to load all ads. Please try again later.');
+        	});
+	}
+
+	$scope.changePage = function changePage(page) {
+		$scope.parameters.startPage = page;
+	}
+}]);
+
+
 /*	if($routeParams.page == undefined) {
 		$scope.startPage = 1;
 	} else {
@@ -31,34 +64,3 @@ adsApp.controller('AdsHomeController', ['$scope', '$routeParams', 'adsDataServic
 	} else {
 		$scope.townsId = $routeParams.townsId;
 	}*/
-	getAllAds();
-
-	$scope.reload = function reload() {
-		$scope.parameters.categoryId = '';
-		$scope.parameters.townId = '';
-		$('#title').text('Home');
-		getAllAds();
-	};
-
-	function getAllAds() {
-		adsDataService.getAllAds($scope.parameters.categoryId, $scope.parameters.townId, $scope.parameters.startPage, $scope.parameters.pageSize, 
-			function(data, status, headers, config) {
-				$scope.alertMsg('success', 'Ads was successfully loaded!');
-				$scope.data = data;
-				$scope.ready = true;
-				$scope.numPages = [];
-
-				var startWithPage = $scope.parameters.startPage - 1;
-				for (var i = 0; i < 5; i++, startWithPage++) {
-					$scope.numPages[i] = startWithPage + 1;
-				};
-			},
-			function (data, status, headers, config) {
-            	$scope.alertMsg('danger', 'Failed to load all ads. Please try again later.');
-        	});
-	}
-
-	$scope.changePage = function changePage(page) {
-		$scope.parameters.startPage = page;
-	}
-}]);
